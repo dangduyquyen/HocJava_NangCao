7ad0471945b37bcb1bcb70d8fd532c7a77abe361
dangduyquyen <83438285+dangduyquyen@users.noreply.github.com>
2022-07-12 20:37:22 +0700

    add foder

---
+++ test_jdbc/src/main/java/com/mycompany/test_jdbc/Student.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test_jdbc;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Student {
+    String ID;
+    String hoTen;
+    String diaChi;
+    String email;
+
+    public Student() {
+    }
+
+    
+    public Student(String ID, String hoTen, String diaChi, String email) {
+        this.ID = ID;
+        this.hoTen = hoTen;
+        this.diaChi = diaChi;
+        this.email = email;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    void getID(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void getHoTen(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void getDiaChi(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void getEmail(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "ID=" + ID + ", hoTen=" + hoTen + ", diaChi=" + diaChi + ", email=" + email + '}';
+    }
+    
+
+    
+}   
